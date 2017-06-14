456d7f37c96c7cf723d1055055fde2e0721820e0
mrossi80 <mrossi80@gmail.com>
2017-06-14 22:12:03 +0200

    custom env

---
+++ misc/mrbs.sh
@@ -0,0 +1,261 @@
+#!/usr/bin/env bash
+
+for arg in "$@"; do
+    if [ "--debug" == "$arg" ]; then
+        set -x
+        set -e
+    fi
+done
+
+BASE_URL="https://github.com/softfire-eu/"
+MANAGERS="experiment-manager nfv-manager physical-device-manager sdn-manager"
+VENV_NAME="$HOME/.softfire"
+SESSION_NAME="softfire"
+CODE_LOCATION="/opt/softfire"
+CONFIG_LOCATION="/etc/softfire"
+CONFIG_FILE_LINKS="https://raw.githubusercontent.com/softfire-eu/experiment-manager/master/etc/experiment-manager.ini \
+https://raw.githubusercontent.com/softfire-eu/nfv-manager/master/etc/nfv-manager.ini \
+https://raw.githubusercontent.com/softfire-eu/nfv-manager/master/etc/available-nsds.json"
+
+function install_requirements {
+    sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+    sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+    sudo apt-get update
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes virtualenv git tmux mysql-server python3-pip build-essential libmysqlclient-dev libssl-dev libffi-dev python-dev
+}
+
+function install_manager() {
+    manager_name=$1
+    if [ "$2" == "--upgrade" ]; then
+        pip install --upgrade ${manager_name}
+    else
+        pip install ${manager_name}
+    fi
+}
+
+function enable_virtualenv {
+  echo "Creating virtualenv"
+  if [ ! -d ${VENV_NAME} ]; then
+    virtualenv --python=python3 ${VENV_NAME}
+   fi
+   echo "created virtual env"
+  . "$VENV_NAME/bin/activate"
+}
+
+function usage {
+    echo "$0 <action>"
+    echo ""
+    echo "actions:    [install|update|clean|start|codestart|codeupdate|codeinstall|clean|purge]"
+    exit 1
+
+}
+
+function create_folders {
+for dir in ${CONFIG_LOCATION} "/var/log/softfire" "${CONFIG_LOCATION}/users"; do
+    if [ ! -d ${dir} ]; then
+        sudo mkdir -p ${dir}
+        sudo chown ${USER} ${dir}
+    fi
+done
+}
+
+function download_gui {
+
+    if [ ! -d "${CONFIG_LOCATION}/views" ]; then
+        pushd /etc/softfire
+        git clone https://github.com/softfire-eu/views.git
+    else
+        pushd /etc/softfire/views
+        git pull
+    fi
+    popd
+}
+
+function copy_config_files {
+    pushd ${CONFIG_LOCATION}
+
+    for url in ${CONFIG_FILE_LINKS}; do
+        file_name=${url##*/}
+        echo "Checking $file_name"
+        if [ ! -f ${file_name} ]; then
+            wget ${url}
+        fi
+    done
+
+    popd
+}
+
+function remove_venv {
+    rm -rf ${VENV_NAME}
+}
+
+function remove_databases {
+    # Works only for sqlite!
+    db_location=$(awk -F ":///" '/^url/ {print $2}' /etc/softfire/experiment-manager.ini)
+    rm -rf ${db_location}
+    db_location=$(awk -F ":///" '/^url/ {print $2}' /etc/softfire/nfv-manager.ini)
+    rm -rf ${db_location}
+
+    echo -n Mysql root Password:
+    read -s mysql_password
+    echo
+    mysql -u root -p${mysql_password} -e "drop database if exists softfire;"
+}
+
+function main {
+
+    if [ "0" == "$#" ]; then
+        usage
+    fi
+    if [ "1" == "$#" -a "--debug" == "$1" ]; then
+        usage
+    fi
+
+    for var in "$@";
+    do
+        case ${var} in
+        "setupenv")
+
+            install_requirements
+            create_folders
+            #pip3 install bottle-cork
+            #python3 generate_cork_files.py "${CONFIG_LOCATION}/users/"
+
+            copy_config_files
+
+            download_gui
+           ;;
+
+         "start")
+            tmux new -d -s ${SESSION_NAME}
+
+            for m in ${MANAGERS}; do
+                echo "Starting ${m}"
+                tmux neww -t ${SESSION_NAME} -n "${m}" "source $VENV_NAME/bin/activate && ${m}"
+                sleep 2
+            done
+
+
+         ;;
+        "install")
+
+            install_requirements
+            create_folders
+            enable_virtualenv
+
+            for m in ${MANAGERS}; do
+                install_manager ${m}
+            done
+
+            python generate_cork_files.py "${CONFIG_LOCATION}/users/"
+
+            copy_config_files
+
+            download_gui
+           ;;
+
+         "start")
+            tmux new -d -s ${SESSION_NAME}
+
+            for m in ${MANAGERS}; do
+                echo "Starting ${m}"
+                tmux neww -t ${SESSION_NAME} -n "${m}" "source $VENV_NAME/bin/activate && ${m}"
+                sleep 2
+            done
+
+
+         ;;
+         "clean")
+            echo -n Mysql root Password:
+            read -s mysql_password
+            echo
+
+            mysql -u root -p${mysql_password} -e "drop database if exists softfire; create database softfire;"
+
+            python generate_cork_files.py
+         ;;
+         "update")
+            enable_virtualenv
+
+            for m in ${MANAGERS}; do
+                install_manager ${m} "--upgrade"
+            done
+
+            download_gui
+         ;;
+         "codeupdate")
+             pushd ${CODE_LOCATION}
+
+             for x in `ls`; do
+                pushd $x && git checkout . && git pull && popd;
+             done
+             popd
+             download_gui
+         ;;
+         "codeinstall")
+            rm -Rf ${CODE_LOCATION}
+            if [ ! -d ${CODE_LOCATION} ]; then
+                sudo mkdir ${CODE_LOCATION}
+                sudo chown -R ${USER} ${CODE_LOCATION}
+            else
+                echo "Folder '/opt/softfire' exists already, delete it before code install"
+                exit 1
+            fi
+
+            pushd /opt/softfire
+            for m in ${MANAGERS}; do
+                git clone "${BASE_URL}${m}.git"
+            done
+
+         ;;
+         "codestart")
+
+            enable_virtualenv
+            for m in ${MANAGERS}; do
+                pip uninstall ${m} -y
+            done
+            deactivate
+
+            tmux new -d -s ${SESSION_NAME}
+
+            for m in ${MANAGERS}; do
+                echo "Starting ${m}"
+                tmux neww -t ${SESSION_NAME} -n "${m}" "source $VENV_NAME/bin/activate && cd ${CODE_LOCATION}/${m} && ./${m}"
+                sleep 2
+            done
+         ;;
+
+         "stop")
+            tmux kill-session -t ${SESSION_NAME}
+         ;;
+
+         "clean")
+            remove_venv
+         ;;
+
+         "purge")
+            read -p "Are you sure you want to purge all (y/n)?" choice
+            case "$choice" in
+              y|Y )
+                remove_venv
+                rm -rf ${CODE_LOCATION}
+                # remove_databases
+                echo "To complete the purging, delete the folder ${CONFIG_LOCATION}"
+                ;;
+              n|N )
+                echo "ah ok..."
+                ;;
+              * )
+                echo "invalid choice"
+                ;;
+            esac
+
+         ;;
+        esac
+
+    done
+
+}
+
+
+main $@
